503993f059bef8e56ca0ddb06fd09605cbb34612
sada <tomo31.ba@gmail.com>
2020-08-20 18:17:12 +0900

    first commit

---
+++ webhook.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = '71yvtBsOWeZBLy7Vu8He4L3SL//BUAVKf15po42SCZmrIhNph9w30qRqBQGmXTU+uef6gt6G9AUf9F9dUepKwZX5xFD0bycJlKKlBvAZpEpcg3EpSCc3TnyYZL25xvMVCKTgIUXJWL7laZDFkdY6iQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'c89ac7a23490966b641f672c73f37917';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
+};
